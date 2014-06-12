e2190b357379434f90c909278d43a6f86d81430a
maycon <maycon@hacknroll.com>
2014-06-12 10:22:49 -0300

    Removed ISR of IDT

--- idt.c
+++ idt.c
@@ -3,6 +3,7 @@
  */
 
 #include <idt.h>
+#include <isr.h>
 #include <string.h>
 
 idt_entry_t idt_entries[IDT_ENTRIES];
@@ -16,4 +17,9 @@ void idt_init()
 	memset(idt_entries, '\0', sizeof(idt_entries));
 
 	IDTR(idt_entries, 0, (uint32)isr0, 0x08, IDT_FLAG_TYPE_INT32);
+	IDTR(idt_entries, 1, (uint32)isr1, 0x08, IDT_FLAG_TYPE_INT32);
+	IDTR(idt_entries, 2, (uint32)isr2, 0x08, IDT_FLAG_TYPE_INT32);
+	IDTR(idt_entries, 3, (uint32)isr3, 0x08, IDT_FLAG_TYPE_INT32);
+
+	set_idt((uint32)&idt);
 }
--- include/idt.h
+++ include/idt.h
@@ -54,40 +54,5 @@ extern void set_idt(uint32);
 
 void idt_init();
 
-/**
- * Interrupt Service Routines (ISR)
- */
-extern void isr0();   // Division by zero exception
-extern void isr1();   // Debug exception
-extern void isr2();   // Non maskable interrupt
-extern void isr3();   // Breakpoint exception
-extern void isr4();   // 'Into detected overflow'
-extern void isr5();   // Out of bounds exception
-extern void isr6();   // Invalid opcode exception
-extern void isr7();   // No coprocessor exception
-extern void isr8();   // Double fault (pushes an error code)
-extern void isr9();   // Coprocessor segment overrun
-extern void isr10();  // Bad TSS (pushes an error code)
-extern void isr11();  // Segment not present (pushes an error code)
-extern void isr12();  // Stack fault (pushes an error code)
-extern void isr13();  // General protection fault (pushes an error code)
-extern void isr14();  // Page fault (pushes an error code)
-extern void isr15();  // Unknown interrupt exception
-extern void isr16();  // Coprocessor fault
-extern void isr17();  // Alignment check exception
-extern void isr18();  // Machine check exception
-extern void isr19();  // Reserved
-extern void isr20();  // Reserved
-extern void isr21();  // Reserved
-extern void isr22();  // Reserved
-extern void isr23();  // Reserved
-extern void isr24();  // Reserved
-extern void isr25();  // Reserved
-extern void isr26();  // Reserved
-extern void isr27();  // Reserved
-extern void isr28();  // Reserved
-extern void isr29();  // Reserved
-extern void isr30();  // Reserved
-extern void isr31();  // Reserved
 
 #endif
