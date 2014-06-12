4a187a75e9be86af24fd0977313da97f4b67cc71
maycon <maycon@hacknroll.com>
2014-06-12 01:38:52 -0300

    Call idt_init()

--- kmain.c
+++ kmain.c
@@ -4,10 +4,12 @@
 
 #include "kconsole.h"
 #include "gdt.h"
-
+#include "idt.h"
+ 
 void kmain(void)
 {
 	gdt_init();
+	idt_init();
 	console_init();
 
 	console_write_text(">>> Hack N' Roll - Kernel v0.0.1 (#1) <<<\n");
